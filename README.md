63fe6778ccd37cf0d8599ce50349bcb05ac617fc
paragb <paragbharg@gmail.com>
2015-01-25 14:53:15 -0500

    Updated README and added twoi additional files.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# GettingAndCleaningData
+The data for the project is downloaded from the following link:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+Unzip the data and ‘cd’ to ‘UCI HAR Dataset’ directory. The ‘R’ script should be located in this directory. 
+
+All the steps which is described below is numbered in the ‘run_analyis.R’ script. Please refer to the ‘R’ code to follow how the script works.
+
+
+Steps:
+1)	Read the column names, select measurements which is required for analysis and provide better descriptive names for the column names.
+	a.	Create a vector of integer to filter out ‘std’ and ‘mean’ which is required for analysis.
+	b.	Create a data.frame – ‘measurement’ of labels which contains ‘std’ and ‘means’ by using the vector from the above step (this will give 66 columns of measurement)
+	c.	Create descriptive names by:
+		i.	removing ‘()-‘ characters.
+		ii.	Fix error in label by removing by fixing ‘BodyBody’ -> ‘Body’
+		iii.	Replace ‘f’ and ‘t’ if they are first character with ‘freq’ abnd ‘time’.
+		iv.	Replace ‘XYX’ character if they are the last character with ‘Xaxis’, ‘Yaxis’ ansd ‘Zaxis’ for better readability of column names.
+		
+2)	Read the subject data and label the columns.
+
+3)	Read the activity and label the columns appropriately.
+
+4)	Read the measurement, for training and test, and filter out the measurement which are required for analysis and label the columns from ‘step-1’ above.
+
+5)	Create a data.frame by combining the subject , activity and measurement (step 2 and 3 above) via column combine function.
+
+6)	Follow steps 2-5 for training and test measurement and combine the data.frame via row combine function.
+
+7)	Create an activity data.frame frame and replace the second  column with descriptive names. I could have used the data provided in 'data_activity.txt' but chose to provide better activity names.
+
+8)	Create a summary with the average of activity and subject for the data fame, by using ‘dplyr’ from step 5 and save to a file.
+	Note: They were two method of creating summary from left to right and inside out. I chose to create the summary by left to right logic.
+	
+9)	Write the ‘summaryOfMeasurement’ to a file.
---
+++ run_analysis.R
@@ -0,0 +1,95 @@
+library(dplyr)
+
+setwd("C:/Users/Parag/coursera/3/UCI HAR Dataset")
+
+## Step 1:
+## Create a data.frame of test measuremnt and improve readability of label by 
+## applying the following tranformation:
+## ONly select test measuremnt for 'std' - stand deviation and mean (average)
+## Remove special character like '()-'
+## Fix label errors - 'BodyBody' -> 'Body'
+## Create meaning full name by replace 'freq' if the first character is 'f' and 
+## 'time' if the foirst caracter is 't'
+## replace 'XYZ' if they are the last charter by 'Xaxis', Yaxis' or 'Zaxis'
+features_url <- "./features.txt"
+features <- read.table(features_url, header = FALSE)
+stdMeanRowNo <- grep("std[(]|mean[(]", features[,2])
+measurements <- features[stdMeanRowNo,]
+measurements[,2] <- gsub("[()]", "", measurements[,2])
+measurements[,2] <- gsub("[-]*mean[-]*", "Mean", measurements[,2])
+measurements[,2] <- gsub("[-]*std[-]*", "Std", measurements[,2])
+measurements[,2] <- gsub("BodyBody", "Body", measurements[,2])
+measurements[,2] <- gsub("^f", "freq", measurements[,2])
+measurements[,2] <- gsub("^t", "time", measurements[,2])
+measurements[,2] <- sub("(X|Y|Z)+$", "_\\1axis", measurements[,2], fixed = FALSE)
+
+## Step 2) 'Read subject which are under training
+subjectTrain_url <- "./train/subject_train.txt"
+subjectTrain <- read.table(subjectTrain_url, header = FALSE)
+colnames(subjectTrain) <- "Subject"
+
+## Step 3) Read 'training' activity
+trainActivity_url <- "./train/Y_train.txt"
+trainActivity <- read.table(trainActivity_url, header = FALSE)
+colnames(trainActivity) <- "Activity"
+
+## Process 'training' measurement,
+## Step 4) Read 'training' measurements and only select cols which are part of measurement
+##    and label the 'training' measurement.
+train_url <- "./train/X_train.txt"
+trainDF <- read.table(train_url, header = FALSE)
+trainDF <- trainDF[, measurements[,1]]
+colnames(trainDF) <- measurements[,2]
+
+## Sep 5) Combine 'Subject', 'trainintActivity' and the tarinDF(measurement)
+subjectActivityTrainDF <- cbind(subjectTrain, trainActivity, trainDF)
+
+#-------------------------------------------------------------------------------
+## Do similar acivity for 'test' measurement' (steps 2 - 5 listed above)
+
+## Step 2) 'Read subject which are under test
+subjectTest_url <- "./test/subject_test.txt"
+subjectTest <- read.table(subjectTest_url, header = FALSE)
+colnames(subjectTest) <- "Subject"
+
+## Step 3) Read 'test' activity
+testActivity_url <- "./test/y_test.txt"
+testActivity <- read.table(testActivity_url, header = FALSE)
+colnames(testActivity) <- "Activity"
+
+## Step 4) Read 'test' measurements and only select cols which are part of measurement
+##    and label the 'test' measurement.
+test_url <- "./test/X_test.txt"
+testDF <- read.table(test_url, header = FALSE)
+testDF <- testDF[, measurements[,1]]
+colnames(testDF) <- measurements[,2]
+
+## Step 5) Combine 'Subject', 'testActivity' and the testDF(measurement)
+subjectActivityTestDF <- cbind(subjectTest, testActivity, testDF)
+
+## Step 6
+## Combine 'training' and 'test' measurement
+combinedMeasurements <- rbind(subjectActivityTrainDF, subjectActivityTestDF)
+
+
+## Step 7
+## Create a DF of activity and replace the the second column of the combined test 
+## measurement of numerical values with meaningful textual values.
+activity <- data.frame(no=c(1:6),
+                       activityName = c("Walking", "WalkUpStairs", 
+                                "WalkDownStairs", "Sitting", "Standing", "Laying"))
+combinedMeasurements[,2] <- activity[combinedMeasurements[,2],2]
+
+
+## Step 8
+## utilize dplyr library to  take the average of each variable for each activity 
+## and subject. I could have utilized the following:
+## "summaryOfMeasurements = summarise_each(group_by(combinedMeasurements, Subject, Activity), funs(mean))"
+# but I chose to do the following since multile operations can be read from 
+# left to right rather then understanding the logic from inside to out. 
+summaryOfMeasurements <- combinedMeasurements %>% group_by(Subject, Activity) %>% 
+                                summarise_each(funs(mean))
+
+## Step 9
+# Write the summary of the result 
+write.table(summaryOfMeasurements, file = "./summaryofMeasurements.txt", row.names=FALSE)
